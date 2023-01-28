e01d44360e58f7b59b81e492e8a6bd8445cc2e6b
Luis Gómez <luis.gomezperez@usp.cue.es>
2023-01-28 10:32:35 +0100

    l

--- ejer 7.py
+++ ejer 7.py
@@ -1,65 +1,56 @@
-import heapq
-from doctest import testmod
+import random
+class Armaduras:
+    def _init_(self):
+        self.legiones = ['FL', 'TF', 'TK', 'CT', 'FN', 'FO']
+        self.codigo
+        self.tabla_hash1 = {}
+        self.tabla_hash2 = {}
+        self.armaduras = []
 
-class Node:
-    def __init__(self, char, freq):
-        self.char = char
-        self.freq = freq
-        self.left = None
-        self.right = None
+    def generar_armaduras(self):
+        for i in range(2000):
+            legión = random.choice(self.legiones)
+            codigo = legión + '-'
+            for j in range(4):
+                codigo += str(random.randint(0, 9))
+            armadura = Armaduras()
+            armadura.codigo(codigo)
+            self.armaduras.append(armadura)
 
-    def __lt__(self, other):
-        return self.freq < other.freq
+    def cargar_armaduras(self):
+        for armadura in self.armaduras:
+            ultimos_digitos = armadura.codigo[-3:]
+            if ultimos_digitos not in self.tabla_hash1:
+                self.tabla_hash1[ultimos_digitos] = []
+            self.tabla_hash1[ultimos_digitos].append(armadura)
 
-def build_huffman_tree(freq_table):
-    heap = []
-    for char, freq in freq_table.items():
-        heap.append(Node(char, freq))
-    heapq.heapify(heap)
-    while len(heap) > 1:
-        left = heapq.heappop(heap)
-        right = heapq.heappop(heap)
-        parent = Node(None, left.freq + right.freq)
-        parent.left = left
-        parent.right = right
-        heapq.heappush(heap, parent)
-    return heap[0]
+            inicial_legion = armadura.codigo[:2]
+            if inicial_legion not in self.tabla_hash2:
+                self.tabla_hash2[inicial_legion] = []
+            self.tabla_hash2[inicial_legion].append(armadura)
 
-def build_code_table(root, code, code_table):
-    if root.char:
-        code_table[root.char] = code
-        return
-    build_code_table(root.left, code + "0", code_table)
-    build_code_table(root.right, code + "1", code_table)
+    def eliminar_armadura(self, codigo):
+        for armadura in self.armaduras:
+            if armadura.codigo == codigo:
+                self.tabla_hash1[armadura.codigo[-3:]].remove(armadura)
+                self.tabla_hash2[armadura.codigo[:2]].remove(armadura)
+                self.armaduras.remove(armadura)
+                return True
+        return False
 
-def compress(message, code_table):
-    compressed_message = ""
-    for char in message:
-        compressed_message += code_table[char]
-    return compressed_message
-
-def decompress(compressed_message, root):
-    message = ""
-    current = root
-    for bit in compressed_message:
-        if bit == "0":
-            current = current.left
+    def obtener_armaduras_por_digitos(self, digitos):
+        if digitos in self.tabla_hash1:
+            return self.tabla_hash1[digitos]
         else:
-            current = current.right
-        if current.char:
-            message += current.char
-            current = root
-    return message
+            return []
 
-if __name__ == "__main__":
-    freq_table = {"A": 0.2, "F": 0.17, "1": 0.13, "3": 0.21, "0": 0.05, "M": 0.09, "T": 0.15}
-    root = build_huffman_tree(freq_table)
-    code_table = {}
-    build_code_table(root, "", code_table)
-    message = "AF13M3T"
-    compressed_message = compress(message, code_table)
-    print("Original message:", message)
-    print("Compressed message:", compressed_message)
-    decompressed_message = decompress(compressed_message, root)
-    print("Decompressed message:", decompressed_message)
-    testmod()
+    def obtener_armaduras_por_legion(self, legion):
+        if legion in self.tabla_hash2:
+            return self.tabla_hash2[legion]
+        else:
+            return []
+
+    def asignar_mision(self, digitos, mision):
+        armadura_digitos = self.obtener_armaduras_por_digitos(digitos)
+        for stormtrooper in armadura_digitos:
+            stormtrooper.asignar_mision(mision)
