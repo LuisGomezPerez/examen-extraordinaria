05c679033c926fe518340aaa78e74974ebec500e
Luis Gómez <luis.gomezperez@usp.cue.es>
2023-01-28 09:50:26 +0100

    l

---
+++ ejer 9.py
@@ -0,0 +1,16 @@
+import networkx as nx
+import itertools
+
+def main():
+    G = nx.Graph()
+
+    planets = ['Alderaan', 'Endor', 'Dagobah', 'Hoth', 'Tatooine', 'Kamino', 'Naboo', 'Mustafar', 'Scarif', 'Bespin', 'planeta1', 'planeta2', 'planeta3', 'planeta4', 'planeta5', 'planeta6', 'planeta7']
+    G.add_nodes_from(planets)
+
+    planet_pairs = list(itertools.combinations(planets, 2))
+    for i, pair in enumerate(planet_pairs):
+        G.add_edge(pair[0], pair[1], weight=i)
+
+
+    T = nx.minimum_spanning_tree(G, algorithm='prim', weight='weight')
+    print(T.edges()
